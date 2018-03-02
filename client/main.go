9450103ecebb786c95988a2326d85d33d87f1fba
Nuttapon Thanitsukkan <nuttapon.t@ku.th>
2018-03-02 17:57:03 +0700

    server/client gRPC in Golang

---
+++ client/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	pb "../proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func argParser(n1 string, n2 string) (int32, int32) {
+	N1, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Cannot parse arge[1]: %s", err)
+	}
+	N2, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatalf("Cannot parse arge[2]: %s", err)
+	}
+	return int32(N1), int32(N2)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("Supply 2 args(n1, n2)")
+	}
+
+	n1, n2 := argParser(os.Args[1], os.Args[2])
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("cannot connect: %s", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewCalculatorClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	addResult, err := client.Add(ctx, &pb.AddRequest{int32(n1), int32(n2)})
+	if err != nil {
+		log.Fatalf("Adding error: %s", err)
+	}
+	log.Printf("%d + %d = %d", n1, n2, addResult.N1)
+
+	subtractResult, err := client.Subtract(ctx, &pb.SubtractRequest{int32(n1), int32(n2)})
+	if err != nil {
+		log.Fatalf("Subtracting error: %s", err)
+	}
+	log.Printf("%d - %d = %d", n1, n2, subtractResult.N1)
+
+	multiplyResult, err := client.Multiply(ctx, &pb.MultiplyRequest{int32(n1), int32(n2)})
+	if err != nil {
+		log.Fatalf("Multiplying error: %s", err)
+	}
+	log.Printf("%d * %d = %d", n1, n2, multiplyResult.N1)
+
+	divideResult, err := client.Divide(ctx, &pb.DivideRequest{int32(n1), int32(n2)})
+	if err != nil {
+		log.Fatalf("Dividing error: %s", err)
+	}
+	log.Printf("%d / %d = %d", n1, n2, divideResult.N1)
+}
---
+++ server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "../proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	protocol = "tcp"
+	port     = ":50051"
+)
+
+type server struct{}
+
+func (s *server) Add(ctx context.Context, in *pb.AddRequest) (*pb.AddReply, error) {
+	return &pb.AddReply{N1: in.N1 + in.N2}, nil
+}
+
+func (s *server) Subtract(ctx context.Context, in *pb.SubtractRequest) (*pb.SubtractReply, error) {
+	return &pb.SubtractReply{N1: in.N1 - in.N2}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, in *pb.MultiplyRequest) (*pb.MultiplyReply, error) {
+	return &pb.MultiplyReply{N1: in.N1 * in.N2}, nil
+}
+
+func (s *server) Divide(ctx context.Context, in *pb.DivideRequest) (*pb.DivideReply, error) {
+	return &pb.DivideReply{N1: in.N1 / in.N2}, nil
+}
+
+func main() {
+	lis, err := net.Listen(protocol, port)
+	if err != nil {
+		log.Fatalf("failed to listen: %s", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCalculatorServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
